9b5b6a876844eb563248d83137c033d3db62aa35
mhatrerashmita <rashmitamhatre59@gmail.com>
2023-12-27 11:05:50 +0530

    sdsd

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//be14a59add7bcd9d154f442b7933b173
+//be14a59add7bcd9d154f442b7933b173
+//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+import './App.css';
+import Weather from './components/WeatherApp/Weather';
+function App() {
+  return (
+    <div className="App">
+    <Weather/>
+    </div>
+  );
+}
+
+export default App;
